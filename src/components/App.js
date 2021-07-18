99b439b5d8cafa6e99dd23f8330f82a281807df6
Chad Parker <63312621+imchad-me@users.noreply.github.com>
2021-07-17 20:21:40 -0600

    upgraded

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
-const App = () => {
-	return (
-		<div className='app'>
-			<h1>React Electron Boilerplate</h1>
-			<p>This is a simple boilerplate for using React with Electron</p>
-		</div>
-	)
+import '../assets/css/App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Electron!</h1>
+
+      <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+    </div>
+  )
 }
 
 export default App
