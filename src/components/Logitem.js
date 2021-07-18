999471ef789cb595a702ae788356095631e74ad5
Chad Parker <63312621+imchad-me@users.noreply.github.com>
2021-07-18 12:03:37 -0600

    priority chips styling

--- src/components/Logitem.js
+++ src/components/Logitem.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import DeleteForeverRoundedIcon from '@material-ui/icons/DeleteForeverRounded';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Chip from '@material-ui/core/Chip';
+import { green, yellow, red } from '@material-ui/core/colors';
 
 const Logitem = ({ log: { _id, priority, user, text, created } }) => {
+  const colorForStatus = priority => {
+    switch (priority) {
+      case 'high':
+        return red;
+      case 'moderate':
+        return yellow;
+      default:
+        return green;
+    }
+  };
+
   return (
     <TableRow>
-      <TableCell>{priority}</TableCell>
+      <TableCell>
+        <Chip
+          label={priority}
+          style={{
+            backgroundColor: colorForStatus(priority)[700],
+            color: 'white',
+            width: '100px',
+          }}
+        ></Chip>
+      </TableCell>
       <TableCell>{text}</TableCell>
       <TableCell>{user}</TableCell>
       <TableCell>{created}</TableCell>
       <TableCell>
-        <DeleteForeverRoundedIcon color='secondary' />
+        <IconButton aria-label='delete' color='secondary'>
+          <DeleteIcon />
+        </IconButton>
       </TableCell>
     </TableRow>
   );
